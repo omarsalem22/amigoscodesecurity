05e04d5c6390adc81080a263498bc1e12d41b8c6
omarsalem22 <omarmohamed25112001@gmail.com>
2026-01-04 07:21:45 +0200

    SecurityConfig

--- src/main/java/com/example/amigoscode/AuthServices/Utils/JwtUtil.java
+++ src/main/java/com/example/amigoscode/AuthServices/Utils/JwtUtil.java
@@ -1,29 +1,42 @@
 package com.example.amigoscode.AuthServices.Utils;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.Map;
 import java.util.function.Function;
 
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 
 @Service
 public class JwtUtil {
 
-    private static final String SECRET_KEY = "IVa5iy5xMue7huxgjNsD3FEhbEog8QaHyKonXtEwYRtJuBNKhHglSTVGuKD5cTZuFAjdILAZvEooY1hhC76004" ;
+    private static final String SECRET_KEY = "IVa5iy5xMue7huxgjNsD3FEhbEog8QaHyKonXtEwYRtJuBNKhHglSTVGuKD5cTZuFAjdILAZvEooY1hhC76004";
 
-    
-    private Key getSignInKey() {
-        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
-        return Keys.hmacShaKeyFor(keyBytes);
+
+    public String generateToken(UserDetails userDetails, Map<String, Object> extraClaims) {
+        return Jwts.builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24)) 
+                .signWith(getSignInKey(), SignatureAlgorithm.HS512)
+                .compact();
     }
- 
+
+
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
-
+    }
+    
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
     }
 
     public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
@@ -32,13 +45,25 @@ public class JwtUtil {
     }
 
     private Claims extractAllClaims(String token) {
-
+      
         return Jwts.parserBuilder()
                 .setSigningKey(getSignInKey())
                 .build()
                 .parseClaimsJws(token)
                 .getBody();
-       
     }
-    public Strin
+
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String email = extractUsername(token);
+        return (email.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
 }
--- src/main/java/com/example/amigoscode/config/JwtAuthenticationFilter.java
+++ src/main/java/com/example/amigoscode/config/JwtAuthenticationFilter.java
@@ -2,6 +2,11 @@ package com.example.amigoscode.config;
 
 import java.io.IOException;
 
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -18,22 +23,34 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
-       private final UserRepository userRepository;
-       private final JwtUtil jwtUtil;
-       
-    
+    private final UserRepository userRepository;
+    private final JwtUtil jwtUtil;
+    private final UserDetailsService userDetailsService;
+
     @Override
-    protected void doFilterInternal( HttpServletRequest request, HttpServletResponse response, FilterChain filterChain )
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
-                final String authHeader = request.getHeader("Authorization");
-                final String jwtToken ;
-                final String userEmail;
-                if(authHeader == null || !authHeader.startsWith("Bearer ")){
-                    filterChain.doFilter(request, response);
-                    return;
-                }
-                jwtToken = authHeader.substring(7);
-                userEmail= jwtUtil.extractUsername(jwtToken);
+        final String authHeader = request.getHeader("Authorization");
+        final String jwtToken;
+        final String userEmail;
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        jwtToken = authHeader.substring(7);
+        userEmail = jwtUtil.extractUsername(jwtToken);
+        if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
+            if (jwtUtil.isTokenValid(jwtToken, userDetails)) {
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                        userDetails,
+                        null,
+                        userDetails.getAuthorities());
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+        }
+        filterChain.doFilter(request, response);
     }
 
 }
